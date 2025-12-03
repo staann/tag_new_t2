d0806302e8d9614c2c69071729184005c0ac1369
staann <gustavoch2005@gmail.com>
2025-12-03 10:35:14 -0300

    criando arquivos CSV a partir de arquivos TXT

--- txtPcsv.py
+++ txtPcsv.py
@@ -1,3 +1,5 @@
+import csv
+
 with open("entradaProj225TAG.txt", 'r') as file:
     arquivo = file.readlines()
 
@@ -38,7 +40,7 @@ for x in arquivo:
 
 #print(lista1)
 print("-----")
-print(lista2)
+#print(lista2)
 
 
 for y in lista1:
@@ -51,12 +53,7 @@ print("-----")
 #print(codigoProjeto)
 #print(numeroVagas)  
 #print(requisitos)
-print("-----")
-print("-----")
-print("-----")
-print("-----")
-print("-----")
-print("-----")
+
 
 
 codigoAluno = []
@@ -70,6 +67,30 @@ for y in lista2:
     nota.append(int(partes[-1]))
     #print(y)
 
-print(codigoAluno)
-print(projetosPreferenciais)
-print(nota)
+#print(codigoAluno)
+#print(projetosPreferenciais)
+#print(nota)
+
+# Criar CSV de Projetos
+with open('projetos.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    writer = csv.writer(csvfile, delimiter=';')
+    writer.writerow(['Codigo_Projeto', 'Numero_Vagas', 'Requisito_Minimo_Nota'])
+    for i in range(len(codigoProjeto)):
+        writer.writerow([codigoProjeto[i], numeroVagas[i], requisitos[i]])
+
+print("Arquivo 'projetos.csv' criado com sucesso!")
+
+# Criar CSV de Alunos
+with open('alunos.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    writer = csv.writer(csvfile, delimiter=';')
+    writer.writerow(['Codigo_Aluno', 'Projeto_Preferencia_1', 'Projeto_Preferencia_2', 'Projeto_Preferencia_3', 'Nota'])
+    for i in range(len(codigoAluno)):
+        # Garantir que temos 3 projetos preferenciais (ou menos)
+        prefs = projetosPreferenciais[i]
+        pref1 = prefs[0] if len(prefs) > 0 else ''
+        pref2 = prefs[1] if len(prefs) > 1 else ''
+        pref3 = prefs[2] if len(prefs) > 2 else ''
+        writer.writerow([codigoAluno[i], pref1, pref2, pref3, nota[i]])
+
+print("Arquivo 'alunos.csv' criado com sucesso!")
+
